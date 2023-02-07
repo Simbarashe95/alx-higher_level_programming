25235f9a820f472d4c49a72a881265d179ce3b69
simbarashe <simbarashechigwada@gmail.com>
2023-02-07 13:08:14 -0800

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
 
-""" Load, add, save  """
+def save_to_json_file(my_obj, filename):
 
-from sys import argv
+    """
 
+    function that writes an Object to a text file, using a JSON representation
 
+    """
 
+    import json
 
+    with open(filename, 'w') as f:
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-
-
-""" Script that adds all arguments to a Python list,
-
-	and then save them to a file """
-
-    try:
-
-        add_i = load_from_json_file('add_item.json')
-
-        save_to_json_file(add_i + argv[1:], 'add_item.json')
-
-            except Exception:
-
-                save_to_json_file(argv[1:], 'add_item.json')
+        json.dump(my_obj, f)
