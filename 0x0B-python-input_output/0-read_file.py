41b66e838c59b20842e01c58c6579f62faccec13
simbarashe <simbarashechigwada@gmail.com>
2023-02-07 11:50:56 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
-"""
-
-Contains the read_file function
-
-"""
-
-
 def read_file(filename=""):
-
-    """""reads a text file(UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
-    print(f.read(), end="")
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+    
+    """
+    with open(filename, 'r') as f:
+        print("{}".format(f.read()), end="")
